4c2a745717862c1b2e3dcd4f0001a320918f834d
bill <47billz@gmail.com>
2018-11-22 21:53:49 +0200

    Fix Renderer

--- Renderer.py
+++ Renderer.py
@@ -7,7 +7,7 @@ class Renderer(mistune.Renderer):
 
     def block_code(self, code, lang):
         if not lang:
-            return '\n<pre><code><blockquotes>%s</blockquotes></code></pre>\n' % \
+            return '\n<pre><code>%s</code></pre>\n' % \
                 mistune.escape(code)
         lexer = get_lexer_by_name(lang, stripall=True)
         formatter = html.HtmlFormatter()
